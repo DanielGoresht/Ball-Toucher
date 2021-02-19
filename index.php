81c3f8ecf4ebc45d1ab9803bd3bb9b3113820738
DanielGoresht <danielgoresht@gmail.com>
2021-02-18 22:44:40 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html style="overflow: hidden">
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.2.0/lib/p5.js"></script>
+  </head>
+  <body>
+    <main>
+    <div style="display:block">
+      <script src="sketch.js"></script>
+    </div>
+    </main>
+  </body>
+</html>

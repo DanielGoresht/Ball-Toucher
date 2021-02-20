f5b25a47f9b3209a185f3e4d46f49b7a11f4e5c6
DanielGoresht <danielgoresht@gmail.com>
2021-02-19 18:12:09 -0700

    Update sketch.js
    collision detection

--- sketch.js
+++ sketch.js
@@ -1,39 +1,44 @@
 
 var balls = [];
 var squares = [];
+const fireworks = [];
+var x = 0;
 
 function preload() {
-	ding = loadSound('ding.mp3');
-	key = loadSound('key.mp3');
+	// ding = loadSound('ding.mp3');
+	// key = loadSound('key.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   masterVolume(0.3);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(255);
-  key.rate(.8);
+//   key.rate(.8);
 
-   if (random(0,100) > 1)
+   if (random(0,100) > 99)
    {
    	createSquare();
    }
 
   if (mouseIsPressed) {
-    createBall(mouseX,mouseY);
+	createBall(mouseX,mouseY);
+	
   }
 
-    	for (let i=0; i < squares.length; i++)
+  
+
+    for (let i=0; i < squares.length; i++)
   	{
   		fill(squares[i].color);
 		noStroke();
 		squares[i].x = squares[i].x + squares[i].xspeed;
 		squares[i].y = squares[i].y + squares[i].yspeed;
 
-
-		rect( squares[i].x,squares[i].y, squares[i].height, squares[i].width);
+		rect(squares[i].x,squares[i].y, squares[i].width, squares[i].height);
   	}
 
   	for (let i=0; i < balls.length; i++)
@@ -46,13 +51,13 @@ function draw() {
 		{
 			balls[i].x = width - balls[i].width/2;
 			balls[i].xspeed *= -1;
-			key.play();
+			// key.play();
 		}
 		if (balls[i].x <= balls[i].width/2)
 		{
 			balls[i].x = balls[i].width/2;
 			balls[i].xspeed *= -1;
-			key.play();
+			// key.play();
 		}
 		balls[i].y = balls[i].y + balls[i].yspeed;
 
@@ -60,23 +65,68 @@ function draw() {
 		{
 			balls[i].y = height - balls[i].height/2;
 			balls[i].yspeed *= -1;
-			key.play();
+			// key.play();
 		}
 		if (balls[i].y <= balls[i].height/2)
 		{
 			balls[i].y = balls[i].height/2;
 			balls[i].yspeed *= -1
-			key.play();
+			// key.play();
 		}
 
-  		ellipse( balls[i].x,balls[i].y, balls[i].height, balls[i].width);
+		//hit dectection
+		for (let j = 0; j < squares.length; j++)
+		{
+
+			// if (balls[i].x + balls[i].width + balls[i].xspeed > squares[j].x && 
+			// 	balls[i].x + balls[i].xspeed < squares[j].x + squares[j].width && 
+			// 	balls[i].y + balls[i].height > squares[j].y && 
+			// 	balls[i].y < squares[j].y + squares[j].height)
+			if (balls[i].x + balls[i].width/2 > squares[j].x - squares[j].width/2 && balls[i].x - balls[i].width/2 < squares[j].x + squares[j].width/2 && balls[i].y + balls[i].height/2 > squares[j].y - squares[j].height/2 && balls[i].y - balls[i].height/2 < squares[j].y + squares[j].height/2) 
+			{
+	
+				// balls[i].yspeed = balls[i].yspeed * -1;
+				//bouncingRectSpeedX *= -1;
+				balls[i].xspeed *= -1;
+				balls[i].x += balls[i].xspeed;
+				balls[i].yspeed *= -1;
+				if (squares[j].yspeed/2 > balls[i].yspeed && balls[i].yspeed < 0)
+				{
+					balls[i].yspeed *= - squares[j].yspeed;
+					balls[i].y += balls[i].yspeed;
+				}
+				else
+				{
+					balls[i].y += balls[i].yspeed;
+				}
+				
+	
+				while (balls[i].x + balls[i].width/2 > squares[j].x - squares[j].width/2 && balls[i].x - balls[i].width/2 < squares[j].x + squares[j].width/2 && balls[i].y + balls[i].height/2 > squares[j].y - squares[j].height/2 && balls[i].y - balls[i].height/2 < squares[j].y + squares[j].height/2) 
+				{
+					balls[i].x += balls[i].xspeed;
+					if (balls[i].y > squares[j].y)
+					{
+						balls[i].y += 1;
+					}
+					else
+					{
+						balls[i].y -= 1;
+					}
+
+
+				}
+				squares[j].color = balls[i].color;
+
+			}
+		}
+
+  		ellipse( balls[i].x,balls[i].y, balls[i].width, balls[i].height);
   
- 	 		balls[i].height -= 0.1;
-  		balls[i].width -= 0.1;
-  		if (balls[i].height > windowHeight-200 || balls[i].width > windowWidth - 200 || balls[i].height < 1)
+ 	 	// balls[i].height -= 0.1;
+  		// balls[i].width -= 0.1;
+  		if (balls[i].height > windowHeight-200 || balls[i].width > windowWidth - 200 || balls[i].height < 1 || balls[i].y > windowHeight)
   		{
   			balls.splice(i, 1);
-  			
   		}
   	}  
 }
@@ -90,9 +140,9 @@ function createBall(x,y)
 	  	width: size,
 	  	x: x,
 	  	y: y,
-	  	xspeed: random(-5,5),
-	  	yspeed: random(-5,5),
-	  	color: color(random(0,200), random(255,255), random(0,200))
+	  	xspeed: random(-1,1),
+	  	yspeed: random(-1,1),
+	  	color: color(random(0,250), random(250,255), random(0,250))
 
 	  }
 	balls.push(ball)
@@ -100,48 +150,28 @@ function createBall(x,y)
 	let playbackRate = map((mouseX + mouseY)/2.5, 0.01, width, 1.3, 0);
   	playbackRate = constrain(playbackRate, 0.01, 4);
 
-    ding.rate(playbackRate);
-	ding.play();
+    // ding.rate(playbackRate);
+	// ding.play();
 }
 
 function createSquare()
 {
-	let wall = random(1,4);
-	wall = Math.round(wall);
-
 	let h = random(30,200);
 	let w = random(30,200);
 	let x;
 	let y;
-	if (wall == 1)
-	{
-			console.log(wall);
-		x = - w;
-		y = random(0, windowHeight);
-	}
-	else if (wall == 2)
-	{
-		x = random(0, windowWidth);
-		y = windowHeight + h;
-	}
-	else if (wall == 3)
-	{
-		x = windowWidth + w;
-		y = random(0,windowHeight);
-	}
-	else if (wall = 4)
-	{
+
 		x = random(0, windowWidth);
 		y = -h*2;
-	}
+	
 		square = 
 	  {
 	  	height: h,
 	  	width: w,
 	  	x: x,
 	  	y: y,
-	  	xspeed: random(-3,3),
-	  	yspeed: random(-3,3),
+	  	xspeed: random(-.2,.2),
+	  	yspeed: random(1,2),
 	  	color: color(255, random(0,100),random(100,200))
 	  }
 	  squares.push(square);

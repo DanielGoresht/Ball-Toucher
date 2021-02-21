ae03fdb2ff100edcd7cca05b70a34c483ae9d946
DanielGoresht <danielgoresht@gmail.com>
2021-02-20 17:21:49 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,9 +3,10 @@ var balls = [];
 var squares = [];
 var debrees = [];
 
-var start = false;
+var game_phase = 0;
 var player_hp = 1;
-var gun;
+var gun = 'updoot';
+var spray_chance = 10;
 var damage_modifier = 1;
 var speed_modifier = 1;
 var size_modifier = 1;
@@ -14,8 +15,12 @@ var spray_speed = 1;
 var square_spawn = 200;
 var notes = [];
 var wind_notes = [];
+var flute_notes = [];
+var themes = [];
 var bongo = []
 var fadeAmount = 2;
+var slingshot_startx;
+var slingshot_starty;
 
 var fade = -10;
 
@@ -35,16 +40,35 @@ function preload() {
 	 notes.push(loadSound('audio/high_f.wav'));
 	 notes.push(loadSound('audio/high_g.wav'));
 
-	 wind_notes.push(loadSound('audio/wind_B.wav'));
-	 wind_notes.push(loadSound('audio/wind_E.wav'));
-	 wind_notes.push(loadSound('audio/wind_G.wav'));
-	 wind_notes.push(loadSound('audio/wind_c.wav'));
-	 wind_notes.push(loadSound('audio/wind_f.wav'));
+	 wind_notes.push(loadSound('audio/high_a_wind.wav'));
+	 wind_notes.push(loadSound('audio/high_b_wind.wav'));
+	 wind_notes.push(loadSound('audio/high_c_wind.wav'));
+	 wind_notes.push(loadSound('audio/high_d_wind.wav'));
+	 wind_notes.push(loadSound('audio/high_e_wind.wav'));
+	 wind_notes.push(loadSound('audio/high_f_wind.wav'));
+	 wind_notes.push(loadSound('audio/high_g_wind.wav'));
+
+	 flute_notes.push(loadSound('audio/A_flute.wav'));
+	 flute_notes.push(loadSound('audio/B_flute.wav'));
+	 flute_notes.push(loadSound('audio/C_flute.wav'));
+	 flute_notes.push(loadSound('audio/D_flute.wav'));
+	 flute_notes.push(loadSound('audio/E_flute.wav'));
+	 flute_notes.push(loadSound('audio/F_flute.wav'));
+	 flute_notes.push(loadSound('audio/G_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_a_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_b_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_c_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_d_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_e_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_f_flute.wav'));
+	 flute_notes.push(loadSound('audio/high_g_flute.wav'));
 
 	 bongo.push(loadSound('audio/bongo1.wav'));
 	 bongo.push(loadSound('audio/bongo2.wav'));
 
-	 title = loadSound('audio/title.wav');
+	 themes.push(loadSound('audio/theme_string.wav'));
+	 themes.push(loadSound('audio/theme_piano.wav'));
+	 themes.push(loadSound('audio/theme_flute.wav'));
 	
 	 // ding = loadSound('audio/ding3.wav');
 	 // hit = loadSound('audio/hit.wav');
@@ -69,10 +93,10 @@ function draw() {
 	background(255);
 
 
-	if (start == false)
+	if (game_phase == 0)
 	{
-	  if (!title.isPlaying()) {
-	      title.play();
+	  if (!themes[0].isPlaying()) {
+	      themes[0].play();
 	  } 
 
 		if (random(0, 200) > 10) {
@@ -102,154 +126,133 @@ function draw() {
 			
 		}
 
-		// if (mouseIsPressed) {
+		if (mouseIsPressed && gun == 'spray_n_pray') {
 
-		// 	createSprayBall();
-		// }
-		//draw squares
+			createSprayBall();
+		}
+
 		drawSquares();
-		//draw debree
-		for (let i = 0; i < debrees.length; i++) {
+		drawDebree();
+		drawBalls();
 
-			fill(debrees[i].color);
-			noStroke();
-			debrees[i].x = debrees[i].x + debrees[i].xspeed;
-			debrees[i].y = debrees[i].y + debrees[i].yspeed;
-
-			rect(debrees[i].x, debrees[i].y, debrees[i].width, debrees[i].height);
-			debrees[i].life --;
-			debrees[i].height -= 0.2;
-			debrees[i].width -= 0.2;
-			
-			//remove squares that are off screen
-			if (debrees[i].width < 0) {
-				debrees.splice(i, 1);
-			}
+		if ( gun == "slingshot" && slingshot_startx != null)
+		{
+			stroke(255, 204, 0);
+			strokeWeight(3);
+			line(mouseX,mouseY, slingshot_startx,slingshot_starty);
 		}
 
-		for (let i = 0; i < balls.length; i++) {
 
-			fill(balls[i].color);
-			noStroke();
-			balls[i].x = balls[i].x + balls[i].xspeed;
-			//bounce.rate(12/balls[i].height);
-			//check for hitting walls
-			if (balls[i].x + balls[i].width / 2 > width) {
-				balls[i].x = width - balls[i].width / 2;
-				balls[i].xspeed *= -1;
-				// bongo[Math.floor(random(0, bongo.length))].play();
-
-			}
-			if (balls[i].x <= balls[i].width / 2) {
-				balls[i].x = balls[i].width / 2;
-				balls[i].xspeed *= -1;
-				// bongo[Math.floor(random(0, bongo.length))].play();
-			}
-			balls[i].y = balls[i].y + balls[i].yspeed;
-
-			if (balls[i].y + balls[i].height / 2 > height) {
-				balls[i].y = height - balls[i].height / 2;
-				balls[i].yspeed *= -1;
-				// bongo[Math.floor(random(0, bongo.length))].play();
-			}
-			if (balls[i].y <= balls[i].height / 2) {
-				balls[i].y = balls[i].height / 2;
-				balls[i].yspeed *= -1
-				// bongo[Math.floor(random(0, bongo.length))].play();
-			}
-
-			//hit dectection
-			for (let j = 0; j < squares.length; j++) {
-				//check for collision anywhere
-				if (balls[i].x + balls[i].width / 2 > squares[j].x - squares[j].width / 2 &&
-					balls[i].x - balls[i].width / 2 < squares[j].x + squares[j].width / 2 &&
-					balls[i].y + balls[i].height / 2 > squares[j].y - squares[j].height / 2 &&
-					balls[i].y - balls[i].height / 2 < squares[j].y + squares[j].height / 2) {
-					//step back and check if it was on the side (left or right)
-					if (balls[i].x + balls[i].width / 2 > squares[j].x - squares[j].width / 2 &&
-						balls[i].x - balls[i].width / 2 < squares[j].x + squares[j].width / 2 &&
-						balls[i].y + balls[i].height / 2 - balls[i].yspeed > squares[j].y - squares[j].height / 2 - squares[j].yspeed &&
-						balls[i].y - balls[i].height / 2 - balls[i].yspeed < squares[j].y + squares[j].height / 2 - squares[j].yspeed) {
-						balls[i].xspeed *= -1;
-						balls[i].x += balls[i].xspeed;
-					}
-					//check if it was top or bottom
-					else {
-						balls[i].yspeed *= -1;
-						if (squares[j].yspeed / 2 > balls[i].yspeed && balls[i].yspeed < 0) {
-							balls[i].yspeed = squares[j].yspeed - balls[i].yspeed;
-							balls[i].y += balls[i].yspeed;
-						}
-						else {
-							balls[i].y += balls[i].yspeed;
-						}
-
-					}
-					while (balls[i].x + balls[i].width / 2 > squares[j].x - squares[j].width / 2 && balls[i].x - balls[i].width / 2 < squares[j].x + squares[j].width / 2 && balls[i].y + balls[i].height / 2 > squares[j].y - squares[j].height / 2 && balls[i].y - balls[i].height / 2 < squares[j].y + squares[j].height / 2) {
-						balls[i].x += balls[i].xspeed;
-						if (balls[i].y > squares[j].y) {
-							balls[i].y += 1;
-						}
-						else {
-							balls[i].y -= 1;
-						}
-
-					}
-					squares[j].hp -= balls[i].damage;
-					if (squares[j].hp > 0)
-					{
-						squares[j].color = color(255, random(((4-squares[j].hp)*50)- 33, (4-squares[j].hp)*50), (4-squares[j].hp)*60);
-
-						 //hit.rate(10/balls[i].height);
-
-						notes[Math.floor(random(0, notes.length))].play();
-					}
-					else
-					{
-						createDebree(squares[j].color, squares[j].x, squares[j].y, squares[j].width);
-						//explode.rate(20/(balls[i].height/2));
-						//explode.play();
-						wind_notes[Math.floor(random(0, wind_notes.length))].play();
-						squares.splice(j, 1);
-					}
-					 balls[i].height /= 2;
-			  		 balls[i].width /= 2;
-				}
-			}
-
-
-			ellipse(balls[i].x, balls[i].y, balls[i].width, balls[i].height);
-			   balls[i].height -= 0.05;
-			   balls[i].width -= 0.05;
-
-			if (balls[i].height > windowHeight - 200 || balls[i].width > windowWidth - 200 || balls[i].height < 1 || balls[i].y > windowHeight) {
-				balls.splice(i, 1);
-			}
-		}
 	}
 }
 
-function createSprayBall() {
-	let size = random(10, 40);
+function mousePressed()
+{
+	if (game_phase == 0)
+	{
+
+		game_phase = 1;
+		themes[0].stop();
+		//empty squares
+		squares = [];
+		setTimeout(reduce(),15000);
+
+	}
+	else
+	{
+		if (gun == "updoot")
+		{
+			//createUpBall();
+			createUpBall();
+		}
+		else if (gun == "slingshot")
+		{
+			slingshot_startx = mouseX;
+			slingshot_starty = mouseY;
+			
+		}	
+	}
+}
+
+function mouseReleased()
+{
+	if (gun == "slingshot")
+	{
+		if ( Math.sqrt(Math.pow((slingshot_startx - mouseX), 2) + Math.pow((slingshot_starty - mouseY),2)) > 100 )
+		{
+			createSlingBall();
+		}
+		
+		slingshot_startx = null;
+		slingshot_starty = null;
+	}
+}
+
+
+function createUpBall() {
+	let size = random(10, 15);
 	ball =
 	{
 		height: size,
 		width: size,
 		x: mouseX,
 		y: mouseY,
-		xspeed: random(-5, 5),
-		yspeed: random(-5, 5),
+		xspeed: random(-0.4, 0.4),
+		yspeed: random(-10, -5),
 		color: color(random(0, 200), random(250, 255), random(0, 200)),
-		damage: 1 * damage_modifier
+		damage: 1 * damage_modifier,
+		decay: 0.05
 
 	}
-	// let playbackRate = map((mouseX + mouseY) / 2.5, 0.01, width, 1.3, 0);
-	// playbackRate = constrain(playbackRate, 0.01, 4);
-	// var rand = Math.floor(random(0, notes.length -1));
-	// notes[rand].rate(playbackRate);
+	notes[Math.floor(random(0, notes.length))].play();
+	balls.push(ball);
+}
+
+function createSlingBall() {
+	let size = random(50, 75);
+	ball =
+	{
+		height: size,
+		width: size,
+		x: mouseX,
+		y: mouseY,
+		xspeed: ((slingshot_startx - mouseX)/windowWidth)*30,
+		yspeed: ((slingshot_starty - mouseY)/windowWidth)*30,
+		color: color(random(0, 200), random(250, 255), random(0, 200)),
+		damage: 2 * damage_modifier,
+		decay: 0.10
+
+	}
 	notes[Math.floor(random(0, notes.length))].play();
 	balls.push(ball);
 
+}
+
+function createSprayBall() {
+	if (random(1,100) < spray_chance)
+	{
+
+		let size = random(5, 30);
+		ball =
+		{
+			height: size,
+			width: size,
+			x: mouseX,
+			y: mouseY,
+			xspeed: random(-5, 5),
+			yspeed: random(-5, 5),
+			color: color(random(0, 50), random(250, 255), random(100, 200)),
+			damage: 1 * damage_modifier,
+			decay: random(0.05, 0.1)
+
+		}
+		// let playbackRate = map((mouseX + mouseY) / 2.5, 0.01, width, 1.3, 0);
+		// playbackRate = constrain(playbackRate, 0.01, 4);
+		// var rand = Math.floor(random(0, notes.length -1));
+		// notes[rand].rate(playbackRate);
+		notes[Math.floor(random(0, notes.length))].play();
+		balls.push(ball);
+	}
 }
 
 function createSquare() {
@@ -300,49 +303,172 @@ function createDebree(p_color, p_x, p_y, width)
 	}
 }
 
-function mousePressed()
-{
-	if (start == false)
-	{
-
-		start = true;
-		//empty squares
-		squares = [];
-		setTimeout(reduce(),15000);
-
-	}
-	else
-	{
-		createSprayBall();
-	}
-	
-}
 
 function drawSquares()
 {
 	//draw squares
 	for (let i = 0; i < squares.length; i++) {
+
+
+
 		if (squares[i].x - squares[i].width / 2 < 0 || squares[i].x + squares[i].width/2 > width) {
 			 squares[i].xspeed *= -1;
 		}
 
-		fill(squares[i].color);
-		noStroke();
+
 		squares[i].x = squares[i].x + squares[i].xspeed;
 		squares[i].y = squares[i].y + squares[i].yspeed;
 
-		rect(squares[i].x, squares[i].y, squares[i].width, squares[i].height);
-
-		//remove squares that are off screen
-		if (squares[i].y > windowHeight + 200) {
+				//remove squares that are off screen
+		if (squares[i].y > windowHeight + squares[i].height/2) {
 			squares.splice(i, 1);
-			//alert("GAME OVER");
+			i--;
+			if(game_phase == 1)
+			{
+				bongo[Math.floor(random(0, bongo.length))].play();
+				//alert("GAME OVER");
+			}			
 		}
-
-		
+		else
+		{		
+			fill(squares[i].color);
+			noStroke();
+			rect(squares[i].x, squares[i].y, squares[i].width, squares[i].height);
+		}		
 	}
 }
 
+function drawDebree()
+{
+	for (let i = 0; i < debrees.length; i++) {
+
+		debrees[i].x = debrees[i].x + debrees[i].xspeed;
+		debrees[i].y = debrees[i].y + debrees[i].yspeed;	
+
+		fill(debrees[i].color);
+		noStroke();
+
+		rect(debrees[i].x, debrees[i].y, debrees[i].width, debrees[i].height);
+		debrees[i].life --;
+		debrees[i].height -= 0.2;
+		debrees[i].width -= 0.2;
+		
+		//remove squares that are off screen
+		if (debrees[i].width < 0) {
+			debrees.splice(i, 1);
+		}
+	}
+}
+
+function drawBalls()
+{
+
+	for (let i = 0; i < balls.length; i++) {
+
+
+		balls[i].x = balls[i].x + balls[i].xspeed;
+		//bounce.rate(12/balls[i].height);
+		//check for hitting walls
+		if (balls[i].x + balls[i].width / 2 > width) {
+			balls[i].x = width - balls[i].width / 2;
+			balls[i].xspeed *= -1;
+			// bongo[Math.floor(random(0, bongo.length))].play();
+
+		}
+		if (balls[i].x <= balls[i].width / 2) {
+			balls[i].x = balls[i].width / 2;
+			balls[i].xspeed *= -1;
+			// bongo[Math.floor(random(0, bongo.length))].play();
+		}
+		balls[i].y = balls[i].y + balls[i].yspeed;
+
+		if (balls[i].y + balls[i].height / 2 > height) {
+			balls[i].y = height - balls[i].height / 2;
+			balls[i].yspeed *= -1;
+			// bongo[Math.floor(random(0, bongo.length))].play();
+		}
+		if (balls[i].y <= balls[i].height / 2) {
+			balls[i].y = balls[i].height / 2;
+			balls[i].yspeed *= -1
+			// bongo[Math.floor(random(0, bongo.length))].play();
+		}
+
+		//hit dectection
+		for (let j = 0; j < squares.length; j++) {
+			//check for collision anywhere
+			if (balls[i].x + balls[i].width / 2 > squares[j].x - squares[j].width / 2 &&
+				balls[i].x - balls[i].width / 2 < squares[j].x + squares[j].width / 2 &&
+				balls[i].y + balls[i].height / 2 > squares[j].y - squares[j].height / 2 &&
+				balls[i].y - balls[i].height / 2 < squares[j].y + squares[j].height / 2) {
+				//step back and check if it was on the side (left or right)
+				if (balls[i].x + balls[i].width / 2 > squares[j].x - squares[j].width / 2 &&
+					balls[i].x - balls[i].width / 2 < squares[j].x + squares[j].width / 2 &&
+					balls[i].y + balls[i].height / 2 - balls[i].yspeed > squares[j].y - squares[j].height / 2 - squares[j].yspeed &&
+					balls[i].y - balls[i].height / 2 - balls[i].yspeed < squares[j].y + squares[j].height / 2 - squares[j].yspeed) {
+					balls[i].xspeed *= -1;
+					balls[i].x += balls[i].xspeed;
+				}
+				//check if it was top or bottom
+				else {
+					balls[i].yspeed *= -1;
+					if (squares[j].yspeed / 2 > balls[i].yspeed && balls[i].yspeed < 0) {
+						balls[i].yspeed = squares[j].yspeed - balls[i].yspeed;
+						balls[i].y += balls[i].yspeed;
+					}
+					else {
+						balls[i].y += balls[i].yspeed;
+					}
+
+				}
+				while (balls[i].x + balls[i].width / 2 > squares[j].x - squares[j].width / 2 && balls[i].x - balls[i].width / 2 < squares[j].x + squares[j].width / 2 && balls[i].y + balls[i].height / 2 > squares[j].y - squares[j].height / 2 && balls[i].y - balls[i].height / 2 < squares[j].y + squares[j].height / 2) {
+					balls[i].x += balls[i].xspeed;
+					if (balls[i].y > squares[j].y) {
+						balls[i].y += 1;
+					}
+					else {
+						balls[i].y -= 1;
+					}
+
+				}
+				squares[j].hp -= balls[i].damage;
+				if (squares[j].hp > 0)
+				{
+					squares[j].color = color(255, random(((4-squares[j].hp)*50)- 33, (4-squares[j].hp)*50), (4-squares[j].hp)*60);
+
+					 //hit.rate(10/balls[i].height);
+
+					flute_notes[Math.floor(random(0, flute_notes.length))].play();
+				}
+				else
+				{
+					createDebree(squares[j].color, squares[j].x, squares[j].y, squares[j].width);
+					//explode.rate(20/(balls[i].height/2));
+					//explode.play();
+					wind_notes[Math.floor(random(0, wind_notes.length))].play();
+					flute_notes[Math.floor(random(0, flute_notes.length))].play();
+					squares.splice(j, 1);
+				}
+				 balls[i].height /= 2;
+		  		 balls[i].width /= 2;
+			}
+		}
+
+		   balls[i].height -= balls[i].decay;
+		   balls[i].width -= balls[i].decay;
+
+		if (balls[i].height > windowHeight - 200 || balls[i].width > windowWidth - 200 || balls[i].height < 1 || balls[i].y > windowHeight) {
+			balls.splice(i, 1);
+			i--;
+		}
+		else
+		{
+
+		fill(balls[i].color);
+		noStroke();
+		ellipse(balls[i].x, balls[i].y, balls[i].width, balls[i].height);
+		}
+	}
+}
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
